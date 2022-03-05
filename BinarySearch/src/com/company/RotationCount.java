7ce5f09d818fc8031c88884a04a2ac9c0896b67c
Sourabh kumar <20010001030@gateway.edu.in>
2022-03-05 22:45:09 +0530

    Added Split_Array_Largest_Sum +1 Ques

--- BinarySearch/src/com/company/RotationCount.java
+++ BinarySearch/src/com/company/RotationCount.java
@@ -1,58 +1,17 @@
 package com.company;
 
-//https://leetcode.com/problems/search-in-rotated-sorted-array/
-
-//Pivot = the largest number in the array
-
-public class RotatedBinarySearch {
+public class RotationCount {
     public static void main(String[] args) {
-    int[] arr = {4, 5, 6, 7, 0, 1, 2};
-        System.out.println(findPivotwithduplicate(arr));
+        int[] arr = {0, 1, 2};
+        System.out.println(countRotations(arr));
     }
 
-    static public int search(int[] nums,int target){
-        int pivot = findPivot(nums);
-
-        //if you did not find the pivot it means array is not rotated
-        //so do normal BS
-        if (pivot == -1){
-            //just do normal BS
-            return binarysearch(nums,target,0,nums.length-1);
-        }
-
-        //if pivot is found , you have to found 2 asc Sorted array
-        if (nums[pivot] ==target ){
-            return pivot;
-        }if (target >= nums[0] ){
-            return binarysearch(nums,target,0,pivot-1);
-        }
-        return binarysearch(nums,target,pivot+1,nums.length-1);
+    private static int countRotations(int[] arr) {
+    int pivot = findPivot(arr);
+    return pivot+1;
     }
 
-    static int binarysearch(int[] arr, int target,int start,int end) {
-
-
-        while (start <= end) {
-            //Find middle element
-
-            //int mid=(start + end)/2; //Here is an error
-            //Might be ( start+end ) > int range , so it give error to fix
-            int mid = start + (end - start) / 2; //to get solve equation, it is same as above
-
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                //Ans
-                return mid;
-            }
-        }
-        //Return when element not found
-        return -1;
-    }
-
-
+    //For not duplicates
     static  int findPivot(int[] arr ){
         int start =0;
         int end=arr.length-1;
@@ -73,6 +32,7 @@ public class RotatedBinarySearch {
     }
 
 //For duplicate numbers
+
     static int findPivotwithduplicate(int[] arr ){
         int start =0;
         int end=arr.length-1;
@@ -102,10 +62,10 @@ public class RotatedBinarySearch {
                 end--;
             }
             //left side is sorted , so pivot is in right side
-             else if (arr[start] < arr[mid] || arr[start] == arr[mid] && arr[mid] > arr[end]){
-                 start=mid+1;
+            else if (arr[start] < arr[mid] || arr[start] == arr[mid] && arr[mid] > arr[end]){
+                start=mid+1;
             }else {
-                 end = mid -1;
+                end = mid -1;
             }
         }
         return -1;
---
+++ BinarySearch/src/com/company/SplitArray.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class SplitArray {
+    public static void main(String[] args) {
+
+    }
+
+    public int splitArray(int[] nums,int m){
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            start = Math.max(start,nums[i]); //in the end of the loop this will contaion the max item from the loop
+            end += nums[i];
+
+        }
+        //binary search
+        while(start < end){
+            //try the middle as potential answer
+            int mid = start+ (end-start)/2;
+
+            //calculate how many pieces you can divide this in this max sum
+            int sum = 0;
+            int pices = 1;
+            for (int num : nums) {
+                if (sum+ num > mid){
+                    //you can not add this in this subarray , make a new one
+                    //say you add this num in new subarray  , then sum = num
+                    sum = num;
+                    pices++;
+                }else {
+                    sum +=num;
+                }
+            }
+            if (pices > m){
+                start = mid+1;
+            }else{
+                end = mid;
+            }
+        }
+        return end; //here start == end
+    }
+}
