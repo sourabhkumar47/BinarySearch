99f2cad4af3a1d3311c174cfd8c0c94deefed2bd
sourabhkumar47 <20010001030@gateway.edu.in>
2022-04-22 15:38:39 +0530

    SortedMatrix Solution added

--- BinarySearch/src/com/company/RowColMatrix_Searching.java
+++ BinarySearch/src/com/company/RowColMatrix_Searching.java
@@ -6,28 +6,28 @@ import java.util.Arrays;
 public class RowColMatrix_Searching {
     public static void main(String[] args) {
 
-        int [][] arr={
-                {10,20,30,40},
-                {15,25,35,45},
-                {28,29,37,49},
-                {33,34,38,50}
+        int[][] arr = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {28, 29, 37, 49},
+                {33, 34, 38, 50}
         };
-        System.out.println(Arrays.toString(search(arr,37)));
+        System.out.println(Arrays.toString(search(arr, 37)));
     }
-    static int[] search(int[][] matrix,int target){
-        int r=0;
-        int c= matrix.length-1;
 
-        while (r < matrix.length && c >= 0){
-            if (matrix[r][c] == target){
-                return new int[]{r,c};
+    static int[] search(int[][] matrix, int target) {
+        int r = 0;
+        int c = matrix.length - 1;
+
+        while (r < matrix.length && c >= 0) {
+            if (matrix[r][c] == target) {
+                return new int[]{r, c};
             }
-            if (matrix[r][c] < target){
+            if (matrix[r][c] < target) {
                 r++;
-            }
-            else
+            } else
                 c--;
         }
-        return new int[]{-1,-1};
+        return new int[]{-1, -1};
     }
 }
