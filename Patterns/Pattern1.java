2e08639f9fd8d77aeac5b7bbb7d1d7fa90a201c3
Sourabh Kumar <sourabh.47512@gmail.com>
2024-07-03 22:54:02 +0530

    added patterns

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -1,7 +1,63 @@
 public class Pattern1 {
     public static void main(String[] args) {
-        pattern30(5);
+//        pattern1one(5);
 
+        int rows = 5;
+        int number = 1;
+
+        for (int i = 1; i <= rows; i++) {
+            // Print spaces before numbers
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Print numbers in ascending order
+            for (int k = 1; k <= i; k++) {
+                System.out.print(number + " ");
+                number++;
+            }
+
+            System.out.println();
+        }
+
+    }
+
+//                   *
+//                  **
+//                 ***
+//                ****
+//               *****
+
+    static void pattern1one(int n) {
+        for (int row = 0; row <n; row++) {
+            int noofspacing = n-row;
+
+            for (int spacing = 0; spacing < noofspacing; spacing++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col <= row ; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+//           *****
+//            ****
+//             ***
+//              **
+//               *
+
+    static void pattern6(int n) {
+        for (int row = 0; row < n; row++) {
+            for (int spacing = 0; spacing < row; spacing++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < n-row ; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     static void pattern30(int n) {
@@ -46,12 +102,15 @@ public class Pattern1 {
 
     static void pattern1(int n) {
         for (int row = 1; row <= n; row++) {
-            for (int col = 1; col <= row; col++) {
+            for (int col = 1; col <= row ; col++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
+
+
+
 
     static void pattern2(int n) {
         for (int row = 1; row <= n; row++) {
