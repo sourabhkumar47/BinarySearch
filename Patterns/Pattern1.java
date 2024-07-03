6c3882835a9fa82e47ba46bb0a3f6bc3cbdd96eb
Sourabh Kumar <sourabh.47512@gmail.com>
2024-07-03 19:30:27 +0530

    added patterns

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -1,12 +1,42 @@
 public class Pattern1 {
     public static void main(String[] args) {
-        pattern5(5);
+        pattern30(5);
 
+    }
+
+    static void pattern30(int n) {
+        for (int row = 0; row <= n; row++) {
+            for (int spaces = 0; spaces < n-row; spaces++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >= 1; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern28(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+            int noOfColEachRow = row > n ? n - (row - n) : row;
+
+            int noOfSpaces = n - noOfColEachRow;
+            for (int spaces = 1; spaces <= noOfSpaces; spaces++) {
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= noOfColEachRow; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
     static void pattern5(int n) {
         for (int row = 1; row <= 2 * n; row++) {
-            int ItemInEachCol = row > n ? n - (row - n ) : row;
+            int ItemInEachCol = row > n ? n - (row - n) : row;
             for (int col = 1; col <= ItemInEachCol; col++) {
                 System.out.print("* ");
             }
