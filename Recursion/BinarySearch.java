2ecd1d1286eb8a2c28324afc9148cba8a355ac4f
Sourabh Kumar <sourabh.47512@gmail.com>
2024-07-05 15:07:31 +0530

    added recursion

--- Recursion/BinarySearch.java
+++ Recursion/BinarySearch.java
@@ -2,7 +2,8 @@ public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {2, 3, 4, 5, 6, 7, 8, 9};
         int target = 4;
-        System.out.println(BSearch(arr, target, 0, arr.length-1));
+        System.out.println(search(arr,5));
+//        System.out.println(BSearch(arr, target, 0, arr.length - 1));
     }
 
     static int BSearch(int[] arr, int target, int start, int end) {
@@ -17,5 +18,27 @@ public class BinarySearch {
             return BSearch(arr, target, start, mid - 1);
         }
         return BSearch(arr, target, mid + 1, end);
+    }
+
+
+    static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start > end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (target == nums[mid]) {
+                return mid;
+            }
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
     }
 }
